58759daebd0967a533a9b3540be35aefe0226b97
Sascha Depold <sascha@depold.com>
2012-10-19 10:05:36 +0200

    added field_sql as class method

--- lib/sequel-bit-fields.rb
+++ lib/sequel-bit-fields.rb
@@ -4,6 +4,14 @@ module Sequel::Plugins
   module BitFields
     def self.configure(model, bit_field_column, bit_fields, options = {})
       bit_fields.each_with_index do |bit_field_name, i|
+        index = 2**i
+
+        model.class.instance_eval do
+          define_method("#{bit_field_name}_sql") do |value=true|
+            "#{bit_field_column} & #{index} #{'!' unless value}= #{index}"
+          end
+        end
+
         model.instance_eval do
           define_method("#{bit_field_name}=") do |value|
             self[bit_field_column] = if value
