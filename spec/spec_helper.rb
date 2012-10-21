f4bc9d6b51c90c1d078c9085d9c768662d8b0e27
Sascha Depold <sascha@depold.com>
2012-10-21 10:19:27 +0200

    fixed path of require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 Bundler.setup(:default, :development)
 
-require 'sequel-bit-fields'
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sequel/plugins/bit_fields.rb')
 
 RSpec.configure do |config|
   DB = Sequel.sqlite
